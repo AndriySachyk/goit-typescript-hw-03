19b4c395e9eb7f281c85e76750b977782bdc4926
AndriySachyk <asachyk1312@gmail.com>
2023-12-04 00:45:54 +0200

    home work done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+
+class Key {
+  private signature: string;
+
+  constructor() {
+    this.signature = Math.random().toString(36).substring(2);
+  }
+
+  getSignature(): string {
+    return this.signature;
+  }
+
+}
+
+
+class Person  {
+
+    constructor(private key: Key){}
+    
+    getKey():Key{
+        return this.key
+    }
+}
+
+
+abstract class House {
+    protected door: boolean
+    protected key: Key
+    protected tenants: Person[] = []
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+    
+    comeIn(person: Person):void{
+        if (this.door) this.tenants.push(person)
+    }
+
+   abstract openDoor(key: Key):void;
+}
+
+
+class MyHouse extends House {
+    
+    constructor(key: Key){
+        super(key)
+    }
+
+    openDoor(key: Key):void{
+        if (key.getSignature() === this.key.getSignature()) this.door = true
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
